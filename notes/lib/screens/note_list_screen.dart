592df57b959a22ad6a3f8628d46f48ca739428eb
Luvianto <126235306+Luvianto@users.noreply.github.com>
2024-05-20 17:07:53 +0700

    Map screen

--- notes/lib/screens/note_list_screen.dart
+++ notes/lib/screens/note_list_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:notes/models/note.dart';
 import 'package:notes/services/note_service.dart';
 import 'package:notes/widgets/note_dialog.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class NoteListScreen extends StatefulWidget {
   const NoteListScreen({super.key});
@@ -84,14 +85,37 @@ class NoteList extends StatelessWidget {
                         },
                         title: Text(document.title),
                         subtitle: Text(document.description),
-                        trailing: InkWell(
-                          onTap: () {
-                            showAlertDialog(context, document);
-                          },
-                          child: const Padding(
-                            padding: EdgeInsets.symmetric(vertical: 10),
-                            child: Icon(Icons.delete),
-                          ),
+                        trailing: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            document.lat != null && document.lng != null
+                                ? InkWell(
+                                    onTap: () {
+                                      openMap(document.lat, document.lng);
+                                    },
+                                    child: const Padding(
+                                      padding:
+                                          EdgeInsets.symmetric(vertical: 10),
+                                      child: Icon(Icons.map),
+                                    ),
+                                  )
+                                : const Padding(
+                                    padding: EdgeInsets.symmetric(vertical: 10),
+                                    child: Icon(
+                                      Icons.map,
+                                      color: Colors.grey,
+                                    ),
+                                  ),
+                            InkWell(
+                              onTap: () {
+                                showAlertDialog(context, document);
+                              },
+                              child: const Padding(
+                                padding: EdgeInsets.symmetric(vertical: 10),
+                                child: Icon(Icons.delete),
+                              ),
+                            ),
+                          ],
                         ),
                       ),
                     ],
@@ -102,6 +126,14 @@ class NoteList extends StatelessWidget {
         }
       },
     );
+  }
+
+  Future<void> openMap(String? lat, String? lng) async {
+    Uri uri =
+        Uri.parse("https://www.google.com/maps/search/?api=1&query=$lat, $lng");
+    if (!await launchUrl(uri)) {
+      throw Exception('Could not launch $uri!');
+    }
   }
 
   showAlertDialog(BuildContext context, Note document) {
